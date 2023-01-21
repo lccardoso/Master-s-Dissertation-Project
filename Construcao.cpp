4d91652b3223d2128f3a2311b72b30afeae31bc2
Leonardo Cardoso <leo_ccardoso@yahoo.com.br>
2023-01-21 16:43:06 -0300

    Addition of new functions

--- Construcao.cpp
+++ Construcao.cpp
@@ -22,4 +22,36 @@ float calcula_demanda(int m, int *d, float porcentagem_d){
 	return porcentagem_d*demand;
 }
 
+/*Cálculo de vetor de cobertura*/
+void vetor_cobertura(int m, int n, int **A, int *y, int *z){	
+	int soma =0;
+	int j;
+	
+	for(int i=0; i<m ;i++){
+		//Inicialmente todas os vertices não são cobertos
+		z[i]=0;
+		for(int j=0; j<n; j++){
+			soma =+ A[i][j]*y[j];
+		}
+	if(soma>0) z[i]=1;	
+	}
+}
+
+/*Calculo da função objetivo*/
+float calcula_fo(int m, int n, float demand, int alpha, int *d, int *c, int *z, int *y){
+	float demand_y=0;
+	float f=0;
+	
+	for (int j=0; j<n;j++){
+		f=f+c[j]*y[j];
+	}
+	for (int i=0; i<m;i++){
+		demand_y=demand_y+d[i]*z[i];
+	}
+	if (demand_y<demand){
+		f=f+alpha*(demand-demand_y);
+	}	
+	return f;
+}
+
 
--- Construcao.h
+++ Construcao.h
@@ -4,5 +4,9 @@ void constroi_solucao(int n, int *y);
 /*Calculo da demanda corrente para o problema*/
 float calcula_demanda(int m, int *d, float porcentagem_d);
 
+/*Cálculo de vetor de cobertura*/
+void vetor_cobertura(int m, int n, int **A, int *y, int *z);
 
+/*Calculo da função objetivo*/
+float calcula_fo(int m, int n, float demand, int alpha, int *d, int *c, int *z, int *y);
 
